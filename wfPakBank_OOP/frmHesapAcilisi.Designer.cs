e74adf80ef6e80721e4ecc392b2e8aec6a7443ff
ranasarikaya <ranasarikaya@gmail.com>
2016-12-01 08:47:54 +0300

    Banka İşlemleri
    Banka Hesap İşlemleri

---
+++ wfPakBank_OOP/frmHesapAcilisi.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace wfPakBank_OOP
+{
+    partial class frmHesapAcilisi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapAc = new System.Windows.Forms.Button();
+            this.cbHesapTurleri = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtBakiye = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtTCKNo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTarih = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHesapNo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblHesapID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesapAc
+            // 
+            this.btnHesapAc.Location = new System.Drawing.Point(249, 421);
+            this.btnHesapAc.Name = "btnHesapAc";
+            this.btnHesapAc.Size = new System.Drawing.Size(114, 30);
+            this.btnHesapAc.TabIndex = 33;
+            this.btnHesapAc.Text = "Hesap Aç";
+            this.btnHesapAc.UseVisualStyleBackColor = true;
+            this.btnHesapAc.Click += new System.EventHandler(this.btnHesapAc_Click);
+            // 
+            // cbHesapTurleri
+            // 
+            this.cbHesapTurleri.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbHesapTurleri.FormattingEnabled = true;
+            this.cbHesapTurleri.Items.AddRange(new object[] {
+            "Vadesiz Hesap",
+            "Vadeli Hesap"});
+            this.cbHesapTurleri.Location = new System.Drawing.Point(213, 358);
+            this.cbHesapTurleri.Name = "cbHesapTurleri";
+            this.cbHesapTurleri.Size = new System.Drawing.Size(196, 24);
+            this.cbHesapTurleri.TabIndex = 32;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(113, 358);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 17);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Hesap Türü";
+            // 
+            // txtBakiye
+            // 
+            this.txtBakiye.Location = new System.Drawing.Point(209, 297);
+            this.txtBakiye.Name = "txtBakiye";
+            this.txtBakiye.Size = new System.Drawing.Size(200, 23);
+            this.txtBakiye.TabIndex = 30;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(110, 297);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 17);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Bakiye";
+            // 
+            // txtTCKNo
+            // 
+            this.txtTCKNo.Location = new System.Drawing.Point(209, 265);
+            this.txtTCKNo.Name = "txtTCKNo";
+            this.txtTCKNo.Size = new System.Drawing.Size(200, 23);
+            this.txtTCKNo.TabIndex = 28;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(110, 265);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 17);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "TCKNo";
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(209, 233);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(200, 23);
+            this.txtSoyadi.TabIndex = 26;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(110, 233);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 17);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Soyadı";
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(209, 201);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(200, 23);
+            this.txtAdi.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(110, 201);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 17);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Adı";
+            // 
+            // lblTarih
+            // 
+            this.lblTarih.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTarih.Location = new System.Drawing.Point(209, 150);
+            this.lblTarih.Name = "lblTarih";
+            this.lblTarih.Size = new System.Drawing.Size(200, 20);
+            this.lblTarih.TabIndex = 22;
+            this.lblTarih.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 150);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Açılış Tarihi";
+            // 
+            // lblHesapNo
+            // 
+            this.lblHesapNo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHesapNo.Location = new System.Drawing.Point(209, 95);
+            this.lblHesapNo.Name = "lblHesapNo";
+            this.lblHesapNo.Size = new System.Drawing.Size(200, 20);
+            this.lblHesapNo.TabIndex = 20;
+            this.lblHesapNo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 95);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Hesap No";
+            // 
+            // lblHesapID
+            // 
+            this.lblHesapID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHesapID.Location = new System.Drawing.Point(209, 62);
+            this.lblHesapID.Name = "lblHesapID";
+            this.lblHesapID.Size = new System.Drawing.Size(200, 20);
+            this.lblHesapID.TabIndex = 18;
+            this.lblHesapID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Hesap ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(410, 204);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 17);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "*";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(410, 236);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 17);
+            this.label10.TabIndex = 35;
+            this.label10.Text = "*";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(410, 271);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 17);
+            this.label11.TabIndex = 36;
+            this.label11.Text = "*";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.Color.Red;
+            this.label12.Location = new System.Drawing.Point(410, 300);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(13, 17);
+            this.label12.TabIndex = 37;
+            this.label12.Text = "*";
+            // 
+            // frmHesapAcilisi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(537, 511);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnHesapAc);
+            this.Controls.Add(this.cbHesapTurleri);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtBakiye);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtTCKNo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTarih);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHesapNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblHesapID);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmHesapAcilisi";
+            this.Text = "Hesap Açılış İşlemleri";
+            this.Load += new System.EventHandler(this.frmHesapAcilisi_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapAc;
+        private System.Windows.Forms.ComboBox cbHesapTurleri;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtBakiye;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtTCKNo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTarih;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHesapNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHesapID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
---
+++ wfPakBank_OOP/frmHesapDokumu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfPakBank_OOP
+{
+    public partial class frmHesapDokumu : Form
+    {
+        public frmHesapDokumu()
+        {
+            InitializeComponent();
+        }
+        public static double bakiye;
+        private void frmHesapDokumu_Load(object sender, EventArgs e)
+        {
+            this.Top = 50;
+            this.Left = 20;
+        }
+        private void btnBul_Click(object sender, EventArgs e)
+        {
+            if (txtHesapNo.Text.Trim() != "")
+            {
+                cHesap h = new cHesap();
+                //h.HesapBilgileriGoster(txtHesapNo, txtAdi, txtSoyadi, txtTCKNo, txtHesapTuru, txtTarih);
+                h = h.HesapBilgileriGoster(txtHesapNo.Text);
+                txtAdi.Text = h.Ad;
+                txtSoyadi.Text = h.Soyad;
+                txtTCKNo.Text = h.Tckno;
+                txtHesapTuru.Text = h.HesapTuru;
+                txtTarih.Text = h.Tarih;
+                cHesapHareket har = new cHesapHareket();
+                har.HesapHareketleriGoster(txtHesapNo.Text, lvHareketler);
+                ToplamlariGoster();
+            }
+        }
+        private void ToplamlariGoster()
+        {
+            double ToplamYatan = 0;
+            double ToplamCekilen = 0;
+            for (int i = 0; i < lvHareketler.Items.Count; i++)
+            {
+                if (lvHareketler.Items[i].SubItems[4].Text == "yatan")
+                    ToplamYatan += Convert.ToDouble(lvHareketler.Items[i].SubItems[3].Text);
+                else if (lvHareketler.Items[i].SubItems[4].Text == "cekilen")
+                    ToplamCekilen += Convert.ToDouble(lvHareketler.Items[i].SubItems[3].Text);
+            }
+            txtToplamYatan.Text = ToplamYatan.ToString();
+            txtToplamCekilen.Text = ToplamCekilen.ToString();
+            txtBakiye.Text = (ToplamYatan - ToplamCekilen).ToString();
+        }
+        private void btnPara_Click(object sender, EventArgs e)
+        {
+            bakiye = Convert.ToDouble(txtBakiye.Text);
+            frmParaIslemleri frm = new frmParaIslemleri();
+            frm.HesapBilgileri(lvHareketler.Items[0].SubItems[0].Text, lvHareketler.Items[0].SubItems[1].Text);
+            frm.ShowDialog();
+            //ParaIslemleri formu kapatıldıktan sonra program burdan çalışmaya devam eder.
+            cHesapHareket har = new cHesapHareket();
+            har.HesapHareketleriGoster(txtHesapNo.Text, lvHareketler);
+            ToplamlariGoster();
+        }
+    }
+}
